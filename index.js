4877d8468feaeee1816fa6d35e029238e8e7cf62
Lisachen0208 <lisa@nixbiosensors.com>
2023-11-10 20:49:42 -0600

    initial commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', hello);
 
 
 // Get the port from the environment, i.e., Heroku sets it
-const port = process.env.PORT || 3000;
+const port = 3000;
 const server = app.listen(port, () => {
      const addr = server.address();
      console.log(`Server listening at http://${addr.address}:${addr.port}`)
